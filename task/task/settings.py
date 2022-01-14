ab3d0ccfaff0b960d1195e3899d35005bb95eb01
djangorobert <djangocharm2020@gmail.com>
2022-01-14 17:47:47 -0600

    added a giignore and .env file

--- task/task/settings.py
+++ task/task/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -21,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'mysecretok'
-
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
